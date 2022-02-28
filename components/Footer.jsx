589d291054dcd0c9f459c5811adf5289f0a7833d
Prabakar Jayaraman <jp.prabakar@gmail.com>
2022-02-28 22:36:07 +1100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,42 @@
+import styles from "../styles/Footer.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.cardL}>
+                <h1 className={styles.title}>AV0CAD0 CREATIVES.</h1>
+                <h1 className={styles.linkTitle}>
+                    <Link href="/contact" className={styles.link} passHref>
+                        <>
+                            <span className={styles.linkText}>WORK WITH US</span>
+                            <Image src="/img/link.png" width="40px" height="40px" alt="" />
+                        </>
+                    </Link>
+                </h1>
+            </div>
+            <div className={styles.cardS}>
+                <div className={styles.cardItem}>
+                    12 ADAAM STREET NY <br/> USA
+                </div>
+                <div className={styles.cardItem}>
+                    CONTACT@AGENCY.DEV <br/> 213 1243 12312
+                </div>
+            </div>
+            <div className={styles.cardS}>
+                <div className={styles.cardItem}>
+                    FOLLOW US:
+                    <br /> __FB __IN __BE __TW
+                </div>
+                <div className={styles.cardItem}>
+                    © 2022 AGENCY INTERACTIVE,
+                    <br /> 
+                    ALL RIGHTS RESERVED
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Intro.jsx
@@ -0,0 +1,29 @@
+import styles from '../styles/Intro.module.css'
+import Image from 'next/image'
+import Circle from './Circle'
+
+const Intro = () => {
+    return (
+        <div className={styles.container}>
+            <Circle backgroundColor="#b0ff49" top="-50vh" left="-50vh" />
+            <Circle backgroundColor="#01c686" right="-40vh" />
+            <div className={styles.card}>
+                <h1 className={styles.title}>
+                    <span className={styles.brand}>AV0CAD0</span> DIGITAL PRODUCT AGENCY
+                </h1>
+                <p className={styles.desc}>
+                    Some text goes here...
+                </p>
+                <button className={styles.button}>DISCOVER</button>
+            </div>
+            <div className={styles.card}>
+                <Image src="/img/Avocado.png" layout="fill" 
+                    objectFit="cover"
+                    alt="" 
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Intro
